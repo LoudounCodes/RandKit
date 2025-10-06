51cb2b052e611ac39422d633748379c4c7182200
David Bock <dbock@javaguy.org>
2025-10-05 22:22:20 -0400

    adds documentation to project

---
+++ docs/index.md
@@ -0,0 +1,6 @@
+# RandKit
+## Adding distribution patterns on top of Java's Random Number Generators.
+
+woohoo.
+
+[Project JavaDoc Documentation](./javadoc/index.html)
---
+++ docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.loudouncodes.randkit.api"},{"l":"org.loudouncodes.randkit.continuous"},{"l":"org.loudouncodes.randkit.discrete"},{"l":"org.loudouncodes.randkit.util"}];updateSearchResults();
